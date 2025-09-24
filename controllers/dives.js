dc088346b5cc350b9743490019ea558ab3c68d5b
PoisonTree8 <poison8t@gmail.com>
2025-09-24 20:23:37 +0300

    building new+create routes

--- controllers/dives.js
+++ controllers/dives.js
@@ -4,7 +4,25 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.get('/', (req, res) => {
+  res.render('dives/index.ejs');
+});
 
+router.get('/new', (req,res) => {
+    res.render('dives/new.ejs');
+});
+
+router.post('/', async (req,res) => {
+try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.push(req.body);
+    await currentUser.save();
+    res.redirect('/');
+}   catch (error) {
+    console.log(error);
+    res.redirect('/');
+}
+});
 
 
 
