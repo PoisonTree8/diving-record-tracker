7635e8c1576fb63a446b5068e912834a7d6de926
PoisonTree8 <poison8t@gmail.com>
2025-10-06 16:53:14 +0300

    deleting users.js route

--- controllers/users.js
+++ controllers/users.js
@@ -12,17 +12,5 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:userId', async (req, res) => {
-  try {
-    const userProfile = await User.findById(req.params.userId);
-    if (!userProfile) {
-      return res.redirect('/users');
-    }
-    res.render('users/show.ejs');
-  } catch (error) {
-    console.log(error);
-    res.redirect('/users');
-  }
-});
 
 module.exports = router
